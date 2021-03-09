d7cf1879a6c2184ff560a10d8bc0b138ede6f1cc
masryanv07 <80355346+masryanv07@users.noreply.github.com>
2021-03-10 02:50:13 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import os,time,sys,shutil
+
+class Main:
+
+	def __init__(self):
+		self.detekos()
+
+	def menu(self):
+		print("""
+		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+		;     H E L L O  G U Y S    ;
+		;---------------------------;
+		;       NAMA SAYA RYANV     ;
+		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
